50dab031340047985330e621699c4d4668a5fbcc
pat0026 <patrick.caparros026@gmail.com>
2023-04-09 02:11:36 +0800

    [Chapter-3] Exploring async and await with web programming

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use futures::join;
 
 use std::vec::Vec;
 use async_std;
-use futures::future::join_all;
+use futures::future::{join_all, try_join};
 
 async fn do_something(number: i8) -> i8 {
     println!("number {} is running", number);
@@ -29,63 +29,49 @@ async fn do_something(number: i8) -> i8 {
     return 2;
 }
 
-fn main() {
-    let now = time::Instant::now();
-    let future_one = do_something(1);
-    let two_seconds = time::Duration::new(2,0);
-    thread::sleep(two_seconds);
-    let outcome = block_on(future_one);
-    println!("time elapsed {:?}", now.elapsed());
-    println!("Here is the outcome: {}", outcome);
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
 
-    let future_two = async {
-        return do_something(2).await
-    };
-    let future_two = block_on(future_two);
-    println!("Here is the outcome: {:?}", future_two);
+async fn utils_one() -> impl Responder {
+    "Utils one reached\n"
+}
 
-    let future_three = async {
-        let outcome_one = do_something(2).await;
-        let outcome_two = do_something(3).await;
-        return outcome_one + outcome_two
-    };
+async fn health() -> impl Responder {
+    "All good\n"
+}
 
-    let future_outcome = block_on(future_three);
-    println!("Here is the outcome: {:?}", future_outcome);
+// #[actix_rt::main]
+// async fn main() -> std::io::Result<()> {
+//     let s1 = HttpServer::new(move || {
+//         App::new().service(web::scope("/utils").route(
+//             "/one", web::get().to(utils_one)))
+//     })
+//     .bind("0.0.0.0:8081")?
+//     .run();
+//     let s2 = HttpServer::new( move || {
+//         App::new().service(web::resource("/health").route(
+//             web::get().to(health)))
+//     })
+//     .bind("0.0.0.0:8080")?
+//     .run();
 
-    let future_four = async {
-        let outcome_one = do_something(2);
-        let outcome_two = do_something(3);
-        let results = join!( outcome_one, outcome_two);
-        return results.0 + results.1
-    };
+//     try_join(s1, s2).await?;
 
-    let now = time::Instant::now();
-    let result = block_on(future_four);
-    println!("tiem elapsed {:?}", now.elapsed());
-    println!("here is the result: {:?}", result);
+//     Ok(())
+// }
 
-    let async_outcome = async {
-        // 1
-        let mut futures_vec = Vec::new();
-        let future_four = do_something(4);
-        let future_five = do_something(5);
-
-        // 2
-        futures_vec.push(future_four); 
-        futures_vec.push(future_five);
-
-        // 3
-        let handles = futures_vec.into_iter().map(
-            async_std::task::spawn).collect::<Vec<_>>();
-        
-        // 4
-        let results = join_all(handles).await;
-        return results.into_iter().sum::<i8>();
-    };
-
-    let now = time::Instant::now();
-    let result = block_on(async_outcome);
-    println!("time elapsed for join vec {:?}", now.elapsed());
-    println!("Here is the result: {:?}", result);
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8081")?
+    .workers(3)
+    .run()
+    .await
 }
