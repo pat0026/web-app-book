d403ccb2ec53685e9b2b06a4d265d6b5164f29ac
pat0026 <patrick.caparros026@gmail.com>
2023-04-08 15:16:48 +0800

    [Chapter-2] Managing structs with factories

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 mod to_do;
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use to_do::to_do_factory;
+use to_do::enums::TaskStatus;
+use to_do::ItemTypes;
 
 fn main() {
-    println!("{}", to_do::enums::TaskStatus::DONE);
-    println!("{}", to_do::enums::TaskStatus::PENDING);
-    let outcome = to_do::enums::TaskStatus::DONE.to_string();
-    println!("{}", outcome);
+    let to_do_item = to_do_factory("washing", TaskStatus::DONE);
 
-    println!();
-
-    let done = Done::new("shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status.stringify());
-
-    let pending = Pending::new("laundry");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status.stringify());
+    match to_do_item {
+        ItemTypes::Done(item) => {
+            println!("{}", item.super_struct.status);
+            println!("{}", item.super_struct.title);
+        },
+        ItemTypes::Pending(item) => {
+            println!("{}", item.super_struct.status);
+            println!("{}", item.super_struct.title)
+        }
+    }
 }
