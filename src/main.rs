63511b0d7af820de2e9afd84e825c6cdd78e0074
pat0026 <patrick.caparros026@gmail.com>
2023-04-08 14:41:30 +0800

    [Chapter-2] Building to-do structs

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,20 @@
-// use std::env;
-use clap::{Arg, App};
+mod to_do;
+use to_do::structs::done::Done;
+use to_do::structs::pending::Pending;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
+    println!("{}", to_do::enums::TaskStatus::DONE);
+    println!("{}", to_do::enums::TaskStatus::PENDING);
+    let outcome = to_do::enums::TaskStatus::DONE.to_string();
+    println!("{}", outcome);
 
-    // let path: &str = &args[0];
+    println!();
 
-    // if path.contains("debug") {
-    //     println!("Debug is running");
-    // } else if path.contains("release") {
-    //     println!("release is running");
-    // } else {
-    //     panic!("The setting is neither debug or release");
-    // }
+    let done = Done::new("shopping");
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status.stringify());
 
-        let app = App::new("booking")
-            .version("1.0")
-            .about("Books in user")
-            .author("Maxwell Flitton");
-        let first_name = Arg::new("first name")
-            .long("f")
-            .takes_value(true)
-            .help("first name of user")
-            .required(true);
-        let last_name = Arg::new("last name")
-            .long("l")
-            .takes_value(true)
-            .help("first name of user")
-            .required(true);
-        let age = Arg::new("age")
-            .long("a")
-            .takes_value(true)
-            .help("age of the user")
-            .required(true);
-
-        let app = app.arg(first_name).arg(last_name).arg(age);
-        
-        let matches = app.get_matches();
-        let name = matches.value_of("first name")
-            .expect("First name is required");
-        let surname = matches.value_of("last name")
-            .expect("Surname is required");
-        let age = matches.value_of("age")
-            .expect("Age is required").parse::<i8>().unwrap();
-
-        println!("{:?}", name);
-        println!("{:?}", surname);
-        println!("{:?}", age);
+    let pending = Pending::new("laundry");
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status.stringify());
 }
