3a7f5fee5037e8bc6ee80ea7d1df0e245d9d1bfc
pat0026 <patrick.caparros026@gmail.com>
2023-04-08 12:27:54 +0800

    [Chapter-2] Interacting with cargo

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,51 @@
-use rand::prelude::*;
-
-/// This function generates a float number using a number
-/// generator passed into the function
-/// 
-/// # Arguments
-/// * generator (&mut ThreadRng): the random number
-/// generator to generate the random number
-/// 
-/// # Returns
-/// (f64): Random number between 0 -> 10
-fn generate_float(generator: &mut ThreadRng) -> f64 {
-    let placeholder: f64 = generator.gen();
-    placeholder * 10.0
-}
-
-/// This trait defines the struct to be a user.
-trait isUser {
-    ///This function proclaims that the struct is a user.
-    /// 
-    /// # Arguments
-    /// None
-    /// 
-    /// # Returns
-    /// (bool) true if user, false if not
-    fn is_user() -> bool {
-        return true;
-    }
-}
-
-/// This struct defines a user.
-/// 
-/// # Attributes
-/// * name (String): the name of the user
-/// * age (i8): the age of the user
-struct User {
-    name: String,
-    age: i8
-}
+// use std::env;
+use clap::{Arg, App};
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let random_number = generate_float(&mut rng);
-    println!("{random_number}")
+    // let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+
+    // let path: &str = &args[0];
+
+    // if path.contains("debug") {
+    //     println!("Debug is running");
+    // } else if path.contains("release") {
+    //     println!("release is running");
+    // } else {
+    //     panic!("The setting is neither debug or release");
+    // }
+
+        let app = App::new("booking")
+            .version("1.0")
+            .about("Books in user")
+            .author("Maxwell Flitton");
+        let first_name = Arg::new("first name")
+            .long("f")
+            .takes_value(true)
+            .help("first name of user")
+            .required(true);
+        let last_name = Arg::new("last name")
+            .long("l")
+            .takes_value(true)
+            .help("first name of user")
+            .required(true);
+        let age = Arg::new("age")
+            .long("a")
+            .takes_value(true)
+            .help("age of the user")
+            .required(true);
+
+        let app = app.arg(first_name).arg(last_name).arg(age);
+        
+        let matches = app.get_matches();
+        let name = matches.value_of("first name")
+            .expect("First name is required");
+        let surname = matches.value_of("last name")
+            .expect("Surname is required");
+        let age = matches.value_of("age")
+            .expect("Age is required").parse::<i8>().unwrap();
+
+        println!("{:?}", name);
+        println!("{:?}", surname);
+        println!("{:?}", age);
 }
