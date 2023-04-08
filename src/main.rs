1eee90016613ac10e4acc1c060e11c7cdc224c26
pat0026 <patrick.caparros026@gmail.com>
2023-04-09 00:09:14 +0800

    [Chapter-3] Understanding asynchronous programming

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod processes;
 mod state;
 mod to_do;
 
-use std::{env, string};
+use std::thread::JoinHandle;
+use std::{env, thread, time};
 
 use serde_json::value::Value;
 use serde_json::Map;
@@ -14,76 +15,48 @@ use to_do::to_do_factory;
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
+fn do_something(number: i8) -> i8 {
+    println!("number {} is running", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    return 2;
 }
 
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| {
-//         println!("http server factory is firing");
-//         App::new()
-//             .route("/", web::get().to(greet))
-//             .route("/{name}", web::get().to(greet))
-//             .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
-//     })
-//     .bind("127.0.0.1:8080")?
-//     .workers(3)
-//     .run()
-//     .await
-// }
-
-fn add_double(closure: fn(i32) -> i32,
-                one: i32, two: i32) -> i32 {
-                    closure(one) + closure(two)
-                }
-
-fn add_doubles(closure: Box<dyn Fn(i32) -> i32>,
-                one: i32, two: i32) -> i32 {
-                    closure(one)  + closure(two)
-                }
-
 fn main() {
-    let test_closure = |string_input| {
-        println!("{}", string_input);
+    // let now = time::Instant::now();
+    // let one: i8 = do_something(1);
+    // let two: i8 = do_something(2);
+    // let three: i8 = do_something(3);
+
+    // println!("time elapsed {:?}", now.elapsed());
+    // println!("result {}", one + two + three);
+
+    let now = time::Instant::now();
+    let thread_one: JoinHandle<i8> = thread::spawn(|| do_something(1));
+    let thread_two: JoinHandle<i8> = thread::spawn(|| do_something(2));
+    let thread_three: JoinHandle<i8> = thread::spawn(|| do_something(3));
+
+    let result_one = thread_one.join();
+    let result_two = thread_two.join();
+    let result_three = thread_three.join();
+
+    println!("time elapsed {:?}", now.elapsed());
+
+    match result_one {
+        Ok(result) => {
+            println!("the result for thread one is {}", result);
+        }
+        Err(ref result) => {
+            if let Some(string) = result.downcast_ref::<String>() {
+                println!("the error for thread one is: {}", string);
+            } else {
+                println!("ther error for thread one does not have a message");
+            }
+        }
     };
-    test_closure("test");
-    // Can't be
-    // test_closure(23);
 
-    println!();
-    // // Can't be
-    // {
-    //     let test_closure_2 = |string_input| {
-    //         println!("{}", string_input);
-    //     };
-    // }
-
-    // // test_closure_2("test");
-
-    println!();
-
-    let another_str = "case";
-    let test_closure_3 = |string_input| {
-        println!("{} {}", string_input, another_str);
-    };
-
-    test_closure_3("test");
-
-    println!();
-
-    let closure = |int_input| int_input * 2;
-    let outcome = add_double(closure, 2, 3);
-    println!("{}", outcome);
-
-    println!();
-
-    let one = 2;
-
-    let closure = move |int_input| {
-        int_input * one
-    };
-    let outcome = add_doubles(Box::new(closure), 2, 3);
-    println!("{}", outcome);
+    println!(
+        "result {}",
+        result_one.unwrap() + result_two.unwrap() + result_three.unwrap()
+    );
 }
