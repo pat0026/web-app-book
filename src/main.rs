73978af01c9d472214fe86924dee66689f74ac2f
pat0026 <patrick.caparros026@gmail.com>
2023-04-09 20:57:24 +0800

    [Chapter-4] Passing parameters into views

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,7 @@ mod state;
 mod to_do;
 mod views;
 
-use std::thread::JoinHandle;
-use std::{env, thread, time, result};
-
-use serde_json::value::Value;
-use serde_json::Map;
-
-use processes::process_input;
-use state::read_file;
-use to_do::enums::TaskStatus;
-use to_do::to_do_factory;
-
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-use futures::executor::block_on;
-use futures::join;
-
-use std::vec::Vec;
-use async_std;
-use futures::future::{join_all, try_join};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,7 +1,12 @@
 mod auth;
+mod to_do; // define the module
+
 use actix_web::web::ServiceConfig;
+
 use auth::auth_views_factory;
+use to_do::to_do_views_factory; // import the factory
 
 pub fn views_factory(app: &mut ServiceConfig) {
     auth_views_factory(app);
+    to_do_views_factory(app); // pass the ServiceConfig
 }
