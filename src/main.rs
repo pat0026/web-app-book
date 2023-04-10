f8cc4bb31ccce1434fb2aebd1da7030ebb417522
pat0026 <patrick.caparros026@gmail.com>
2023-04-10 15:49:40 +0800

    [Chapter-4] Extracting data from the header in requets

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
+use actix_service::Service;
+use actix_web::{App, HttpServer};
+
+mod json_serialization;
 mod processes;
 mod state;
 mod to_do;
 mod views;
-mod json_serialization;
-
-use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let app = App::new()
+            .wrap_fn(|req, srv| {
+                println!("{:?}", req);
+                let future = srv.call(req);
+
+                async {
+                    let result = future.await?;
+                    Ok(result)
+                }
+            })
             .configure(views::views_factory);
         app
     })
