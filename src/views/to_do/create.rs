45003b444d310858c6ea72d734f1b319ffd5bdb5
pat0026 <patrick.caparros026@gmail.com>
2023-04-12 23:45:07 +0800

    [Chapter-5] Communicating with our server using JavaScript

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,13 +1,14 @@
 use serde_json::value::Value;
 use serde_json::Map;
-use actix_web::HttpRequest;
+use actix_web::{HttpRequest, HttpResponse};
 
 use crate::to_do;
 use crate::to_do::{to_do_factory, enums::TaskStatus};
+use crate::json_serialization::to_do_items::ToDoItems;
 use crate::state::read_file;
 use crate::processes::process_input;
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json");
 
     let title: String = req.match_info().get("title").unwrap().to_string();
@@ -16,5 +17,5 @@ pub async fn create(req: HttpRequest) -> String {
 
     process_input(item, "create".to_string(), &state);
 
-    format!("{} created", title)
+    HttpResponse::Ok().json(ToDoItems::get_state())
 }
