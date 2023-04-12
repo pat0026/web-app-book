d080eebebbe496ab593b04334ac0daf5aa671af6
pat0026 <patrick.caparros026@gmail.com>
2023-04-13 00:51:11 +0800

    [Chapter-5] Adding a JavaScript loading function

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -8,7 +8,12 @@ use super::content_loader::read_file;
 // }
 
 pub async fn items() -> HttpResponse {
-    let html_data = read_file("./templates/main.html");
+    let mut html_data = read_file("./templates/main.html");
+
+    let javascript_data = read_file("./javascript/main.js");
+
+    html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data);
+    
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html_data)
