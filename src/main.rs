6103e56edfc64ee5dbf8d361e4cbaa505e2cfa45
pat0026 <patrick.caparros026@gmail.com>
2023-04-22 22:52:17 +0800

    [Personal] Change vh to percent. Bind to 0.0.0.0 ip

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .configure(views::views_factory);
         app
     })
-    .bind("127.0.0.1:8000")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await
 }
