129b33c477c230c31efe4695d9325d1fbd2e0a65
pat0026 <patrick.caparros026@gmail.com>
2023-04-27 01:15:20 +0800

    [Chapter-6] Inserting into the database

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,21 +1,29 @@
-use serde_json::value::Value;
-use serde_json::Map;
+use crate::diesel;
+use diesel::prelude::*;
+
 use actix_web::{HttpRequest, HttpResponse};
 
-use crate::to_do;
-use crate::to_do::{to_do_factory, enums::TaskStatus};
+use crate::database::establish_connection;
+use crate::models::items::{new_item::NewItem, item::Item};
+use crate::schema::to_do;
 use crate::json_serialization::to_do_items::ToDoItems;
-use crate::state::read_file;
-use crate::processes::process_input;
 
 pub async fn create(req: HttpRequest) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json");
-
     let title: String = req.match_info().get("title").unwrap().to_string();
+    
+    let connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(&title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
 
-    let item = to_do_factory(title.as_str(), TaskStatus::PENDING);
-
-    process_input(item, "create".to_string(), &state);
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&connection);
+    }
 
     HttpResponse::Ok().json(ToDoItems::get_state())
 }
