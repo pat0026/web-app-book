a5c6321aa2a05907266436f677483a36a3e42be1
pat0026 <patrick.caparros026@gmail.com>
2023-04-27 01:28:28 +0800

    [Chapter-6] Editing the database

--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -1,38 +1,25 @@
+use crate:: diesel;
+use diesel::prelude::*;
+
 use actix_web::{web, HttpResponse};
-use serde_json::value::Value;
-use serde_json::Map;
 
-use crate::state::read_file;
-
-use crate::to_do;
-use crate::to_do::{to_do_factory, enums::TaskStatus};
 use crate::json_serialization::{to_do_item::ToDoItem,
                                 to_do_items::ToDoItems};
-use crate::processes::process_input;
 use crate::jwt::JWToken;
+use crate::database::establish_connection;
+use crate::schema::to_do;
 
 pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JWToken) -> HttpResponse {
     println!("here is the message in the token: {}", token.message);
-    let state: Map<String, Value> = read_file("./state.json");
+    let connection = establish_connection();
+    let results = to_do::table
+        .filter(to_do::columns::title
+            .eq(&to_do_item.title));
+    
+    diesel::update(results)
+            .set(to_do::columns::status.eq("DONE"))
+            .execute(&connection);
 
-    let status: TaskStatus;
-    match state.get(&to_do_item.title) {
-        Some(result) => {
-            status = TaskStatus::from_string(result.as_str().unwrap().to_string());
-            let existing_item = 
-                to_do_factory(to_do_item.title.as_str(), status.clone());
-            if status.to_string() == to_do_item.status {
-                return HttpResponse::Ok().json(ToDoItems::get_state());
-            } else {
-                process_input(existing_item, "edit".to_owned(), &state);
-                return HttpResponse::Ok().json(ToDoItems::get_state());
-            }
-        },
-        None => {
-            return HttpResponse::NotFound().json(
-                format!("{} not in state", &to_do_item.title)
-            );
-        }
-    }
+    return HttpResponse::Ok().json(ToDoItems::get_state())
 }
 
