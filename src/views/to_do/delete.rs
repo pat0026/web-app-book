4ecc10d623e148f046d3840fede7fcc5bee1409b
pat0026 <patrick.caparros026@gmail.com>
2023-04-27 02:08:05 +0800

    [Chapter-6] Deleting Data

--- src/views/to_do/delete.rs
+++ src/views/to_do/delete.rs
@@ -1,31 +1,23 @@
-use std::fmt::format;
-use std::result;
+use diesel;
+use diesel::prelude::*;
 
 use actix_web::{web, HttpResponse};
-use serde_json::value::Value;
-use serde_json::Map;
+use crate::database::establish_connection;
+use crate::schema::to_do;
 
 use crate::json_serialization::{to_do_item::ToDoItem, to_do_items::ToDoItems};
 use crate::jwt::JWToken;
-use crate::processes::process_input;
-use crate::state::read_file;
-use crate::to_do::{enums::TaskStatus, to_do_factory};
+use crate::models::items::item::Item;
 
 pub async fn delete(to_do_item: web::Json<ToDoItem>, token: JWToken) -> HttpResponse {
-    let state = read_file("./state.json");
+    let connection = establish_connection();
 
-    let status: TaskStatus;
-    match state.get(&to_do_item.title) {
-        Some(result) => {
-            status = TaskStatus::from_string(result.as_str().unwrap().to_string());
-        }
-        None => {
-            return HttpResponse::NotFound().json(format!("{} not in state", &to_do_item.title));
-        }
-    }
+    let item = to_do::table
+        .filter(to_do::columns::title.eq(&to_do_item.title.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
 
-    let existing_item = to_do_factory(to_do_item.title.as_str(), status);
-    process_input(existing_item, "delete".to_string(), &state);
-
+    diesel::delete(&item[0]).execute(&connection);
     HttpResponse::Ok().json(ToDoItems::get_state())
 }
