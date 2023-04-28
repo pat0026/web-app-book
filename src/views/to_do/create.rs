6cfe03cd1c56916565f300a2b8ebd652e68bc72d
pat0026 <patrick.caparros026@gmail.com>
2023-04-28 23:20:36 +0800

    [Chapter-6] Building a database connection pool

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -3,27 +3,26 @@ use diesel::prelude::*;
 
 use actix_web::{HttpRequest, HttpResponse};
 
-use crate::database::establish_connection;
+use crate::database::DB;
 use crate::models::items::{new_item::NewItem, item::Item};
 use crate::schema::to_do;
 use crate::json_serialization::to_do_items::ToDoItems;
 
-pub async fn create(req: HttpRequest) -> HttpResponse {
+pub async fn create(req: HttpRequest, db: DB) -> HttpResponse {
     let title: String = req.match_info().get("title").unwrap().to_string();
     
-    let connection = establish_connection();
     let items = to_do::table
         .filter(to_do::columns::title.eq(&title.as_str()))
         .order(to_do::columns::id.asc())
-        .load::<Item>(&connection)
+        .load::<Item>(&db.connection)
         .unwrap();
 
     if items.len() == 0 {
         let new_post = NewItem::new(title);
         diesel::insert_into(to_do::table)
             .values(&new_post)
-            .execute(&connection);
+            .execute(&db.connection);
     }
 
-    HttpResponse::Ok().json(ToDoItems::get_state())
+    HttpResponse::Ok().json(ToDoItems::get_state(db))
 }
--- src/views/to_do/edit.rs
+++ src/views/to_do/edit.rs
@@ -6,20 +6,18 @@ use actix_web::{web, HttpResponse};
 use crate::json_serialization::{to_do_item::ToDoItem,
                                 to_do_items::ToDoItems};
 use crate::jwt::JWToken;
-use crate::database::establish_connection;
+use crate::database::DB;
 use crate::schema::to_do;
 
-pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JWToken) -> HttpResponse {
-    println!("here is the message in the token: {}", token.message);
-    let connection = establish_connection();
+pub async fn edit(to_do_item: web::Json<ToDoItem>, token: JWToken, db:DB) -> HttpResponse {
     let results = to_do::table
         .filter(to_do::columns::title
             .eq(&to_do_item.title));
     
     diesel::update(results)
             .set(to_do::columns::status.eq("DONE"))
-            .execute(&connection);
+            .execute(&db.connection);
 
-    return HttpResponse::Ok().json(ToDoItems::get_state())
+    return HttpResponse::Ok().json(ToDoItems::get_state(db))
 }
 
