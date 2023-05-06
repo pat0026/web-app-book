3b8904c39dd30929b6fa75446bc49dd681db21b5
pat0026 <patrick.caparros026@gmail.com>
2023-05-06 16:27:27 +0800

    [Chapter-7] Managing user sessions

--- src/views/auth/login.rs
+++ src/views/auth/login.rs
@@ -1,3 +1,33 @@
-pub async fn login() -> String {
-    format!("Login view")
+use crate::diesel;
+use diesel::prelude::*;
+use actix_web::{web, HttpResponse, Responder};
+
+use crate::database::DB;
+use crate::models::user::user::User;
+use crate::json_serialization::login::Login;
+use crate::schema::users;
+use crate::jwt::JWToken;
+
+pub async fn login(credentials: web::Json<Login>, db: DB) -> impl Responder {
+    let password = credentials.password.clone();
+    let users = users::table
+        .filter(users::columns::username.eq(
+            credentials.username.clone()
+        )).load::<User>(&db.connection).unwrap();
+    
+    if users.len() == 0 {
+        return HttpResponse::NotFound()
+    } else if users.len() > 1 {
+        return HttpResponse::Conflict()
+    }
+
+    match users[0].verify(password) {
+        true => {
+            let token = JWToken::new(users[0].id);
+            let raw_token = token.encode();
+            HttpResponse::Ok().append_header(("token",raw_token)).take()
+        },
+        false => HttpResponse::Unauthorized()
+    }
 }
+
